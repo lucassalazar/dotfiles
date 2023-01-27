8b6e0175efa23dadf22f68ae2edd82ed09ba6c42
lucassalazar <lucasrsalazar@gmail.com>
2023-01-27 03:40:01 -0300

    Remapping harpoon

--- .config/nvim/lua/lucas/plugin/treesitter.lua
+++ .config/nvim/lua/lucas/plugin/treesitter.lua
@@ -5,7 +5,6 @@ if not setup then
 end
 
 treesitter.setup {
-    -- A list of parser names, or "all" (the four listed parsers should always be installed)
     ensure_installed = {
         "javascript",
         "typescript",
