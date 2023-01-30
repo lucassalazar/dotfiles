caa6046cd569836bffac3e5d67f908598aa413d4
lucassalazar <lucasrsalazar@gmail.com>
2023-01-29 22:58:15 -0300

    forcing nvim-tree to open on start up

--- .config/nvim/lua/lucas/plugin/nvim-tree.lua
+++ .config/nvim/lua/lucas/plugin/nvim-tree.lua
@@ -15,7 +15,6 @@ vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeToggle)
 vim.cmd([[ highlight NvimTreeIndentMarker guifg=#ebcb8b ]])
 
 nvimtree.setup({
-    open_on_setup = true,
     sort_by = "case_sensitive",
     view = {
         width = 30,
@@ -47,5 +46,29 @@ nvimtree.setup({
         dotfiles = false
     }
 })
+
+-- Opens nvim on startup
+local function open_nvim_tree(data)
+
+    -- buffer is a [No Name]
+    local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+
+    -- buffer is a directory
+    local directory = vim.fn.isdirectory(data.file) == 1
+
+    if not no_name and not directory then
+        return
+    end
+
+    -- change to the directory
+    if directory then
+        vim.cmd.cd(data.file)
+    end
+
+    -- open the tree
+    require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 
 pcall(vim.cmd, "hi NvimTreeNormal guibg=none")
