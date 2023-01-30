73ca3a5a7cf3e6a2843136d0c21bd6e577d6eaee
lucassalazar <lucasrsalazar@gmail.com>
2023-01-30 00:03:34 -0300

    remapping harpoon

--- .config/nvim/lua/lucas/plugin/harpoon.lua
+++ .config/nvim/lua/lucas/plugin/harpoon.lua
@@ -10,10 +10,10 @@ if not setup_ui then
 	return
 end
 
-vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+vim.keymap.set("n", "<M-a>", mark.add_file)
+vim.keymap.set("n", "<M-e>", ui.toggle_quick_menu)
 
-vim.keymap.set("n", "<C-h>", function () ui.nav_file(1) end)
-vim.keymap.set("n", "<C-j>", function () ui.nav_file(2) end)
-vim.keymap.set("n", "<C-k>", function () ui.nav_file(3) end)
-vim.keymap.set("n", "<C-l>", function () ui.nav_file(4) end)
+vim.keymap.set("n", "<M-h>", function () ui.nav_file(1) end)
+vim.keymap.set("n", "<M-j>", function () ui.nav_file(2) end)
+vim.keymap.set("n", "<M-k>", function () ui.nav_file(3) end)
+vim.keymap.set("n", "<M-l>", function () ui.nav_file(4) end)
